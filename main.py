5d9f30ded38f450c33256ccdadc4719edcfda8f9
ivanko <dxd.vgv@gmail.com>
2023-04-12 02:06:34 +0600

    rabota

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template, redirect, url_for, session
+import json
+import uuid
+
+from flask import Flask, render_template, redirect, url_for, session, request
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'f4fd3b960ef03a8d03f2bdb12133201d35fe0481'
@@ -7,7 +11,8 @@ app.config['SECRET_KEY'] = 'f4fd3b960ef03a8d03f2bdb12133201d35fe0481'
 @app.route('/')
 def main():
     is_login()
-    return render_template('index.html')
+    config = open_config()
+    return render_template('index.html', conf=config['satats'])
 
 
 @app.route('/login')
@@ -26,12 +31,48 @@ def registration():
     return render_template('reg.html')
 
 
+@app.route('/user_add')
+def user_add():
+    config = open_config()
+    config['auth'].update({"login": request.form['login'], "passwd": request.form['passwd']})
+    write_config(config)
+    session['login'] = True
+    return redirect(url_for('main'))
+
+
 @app.route('/panel')
 def admin_panel():
+    config = open_config()
     is_login()
     if not session['login']:
         return redirect(url_for('main'))
-    return render_template('panel.html')
+    return render_template('panel.html', conf=config['satats'])
+
+
+@app.route('/add_state', methods=['POST'])
+def add_state():
+    config = open_config()
+    lastname = secure_filename(request.files['file'].filename).split('.')[-1]
+    filename = str(uuid.uuid4()) + '.' + lastname
+    request.files['file'].save('static/imgs/' + filename)
+
+    config['satats'].update({
+        "satate_" + str(len(config['satats'])): {
+            "titel": request.form['titel'],
+            "img": filename,
+            "text": request.form['text']
+        }
+    })
+    write_config(config)
+    return redirect(url_for('main'))
+
+
+@app.route('/delete_state/<path:state>', methods=['POST'])
+def delete_state(state):
+    config = open_config()
+    config['satate'].pop(state)
+    write_config(config)
+    return '200'
 
 
 def is_login():
@@ -39,10 +80,16 @@ def is_login():
         session['login'] = False
 
 
-@app.route('/logged')
+@app.route('/logged', methods=['POST'])
 def logged():
-    session['login'] = True
-    return redirect(url_for('admin_panel'))
+    config = open_config()
+    try:
+        if config['auth']['login'] == request.form['login'] and config['auth']['passwd'] == request.form['passwd']:
+            session['login'] = True
+            return redirect(url_for('admin_panel'))
+    except:
+        print('fatal')
+    return redirect(url_for('main'))
 
 
 @app.route('/logout')
@@ -51,5 +98,17 @@ def logout():
     return redirect(url_for('main'))
 
 
+def open_config() -> dict:
+    with open('config.json', encoding='utf-8') as f:
+        config = json.load(f)
+
+    return config
+
+
+def write_config(data):
+    with open('config.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False)
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
